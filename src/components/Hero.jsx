6f6964f1ae033775b61eba5147699fe4fd88742a
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-07-01 01:23:32 +0300

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="hero-card">
+            <img src="https://n1s1.hsmedia.ru/13/a5/b2/13a5b2373d5e23489d9a4949ada5b927/547x397_0xac120002_8752067681540468870.jpg" alt="betman" />
+        </div>
+    )
+}
+
+export default Hero;
